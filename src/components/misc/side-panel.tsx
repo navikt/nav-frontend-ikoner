e9566735b3708e263fcb1f338ebd3c7f9f197119
Morten Jansrud <morten.jansrud@bekk.no>
2018-06-26 16:15:23 +0200

    [FO-1227] Refactor

--- src/components/misc/side-panel.tsx
+++ src/components/misc/side-panel.tsx
@@ -6,8 +6,10 @@ class SidePanel extends React.Component {
 
     public render() {
         return (
-            <div className="side-panel">
-                <h2>ToDo: Informasjon</h2>
+            <div className="container-side-panel">
+                <div className="side-panel">
+                    <h2>ToDo: Informasjon</h2>
+                </div>
             </div>
         );
     }
