7f15ca688b00941db296c341f033ab7c6ef1dfe1
Morten Jansrud <morten.jansrud@bekk.no>
2018-07-03 13:45:04 +0200

    FO-1234 Refactored button

--- src/components/buttons/download-button.tsx
+++ src/components/buttons/download-button.tsx
@@ -8,8 +8,8 @@ class DownloadButton extends React.Component <PropTypes> {
 
     public render() {
         return (
-            <a download={true} href={`${config.NAV_ICONS_API_LINK}/icon/download?title=${this.props.icon.title}`} className="icon-download-link">
-                <button type="submit" className="knapp knapp--hoved icon-download-button">Last ned ikon</button>
+            <a download={true} href={`${config.NAV_ICONS_API_LINK}/icon/download?title=${this.props.icon.title}`} className="knapp knapp--hoved icon-download-button">
+                Last ned ikon
             </a>
         );
     }
