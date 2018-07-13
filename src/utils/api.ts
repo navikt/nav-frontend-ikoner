7d574385c4419dc2a96a91b01264140d06237ec2
Morten Jansrud <morten.jansrud@bekk.no>
2018-07-13 08:16:00 +0200

    FO-1273 Fixed wrong commit

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -14,6 +14,16 @@ import LinkCreator from './api-link-creator';
 const debounce = require('lodash.debounce'); // tslint:disable-line
 
 // Create debouncer to prevent user from calling API too frequent
+function fetchIcons(iconStyle: IconStyle,
+                    fetchFrom: number,
+                    fetchTo: number,
+                    searchText: SearchText) :
+    (dispatch: Redux.Dispatch<ReceiveIconsAction>) => Promise<ReceiveIconsAction> {
+    return (dispatch: Redux.Dispatch<ReceiveIconsAction>) => {
+        return fetchIconsDispatchDebounced(dispatch, iconStyle, fetchFrom, fetchTo, searchText);
+    };
+}
+
 const fetchIconsDispatchDebounced = debounce(fetchIconsDispatch, 100);
 function fetchIconsDispatch(dispatch: Redux.Dispatch<ReceiveIconsAction>,
                             iconStyle: IconStyle,
@@ -24,18 +34,11 @@ function fetchIconsDispatch(dispatch: Redux.Dispatch<ReceiveIconsAction>,
     // Build parameters
     const iStyle = LinkCreator.iconStyle(iconStyle);
     const iSearch = LinkCreator.searchText(searchText);
-    const fetchInterval = LinkCreator.fetchInterval(fetchFrom, fetchTo);
-    return fetch  (`${Config.NAV_ICONS_API_LINK}/icons?${iStyle}${iSearch}${fetchInterval}`)
+    const iFetchInterval = LinkCreator.fetchInterval(fetchFrom, fetchTo);
+    return fetch  (`${Config.NAV_ICONS_API_LINK}/icons?${iStyle}${iSearch}${iFetchInterval}`)
         .then(response => response.json())
         .catch(error => console.log(Language.AN_ERROR_HAS_ACCURED, error))
         .then(json => dispatch(receiveIcons(json.icons, json.numberOfIcons)));
-}
-
-function fetchIcons(iconStyle: IconStyle, fetchFrom: number, fetchTo: number,
-                    searchText: SearchText) : (dispatch: Redux.Dispatch<ReceiveIconsAction>) => Promise<ReceiveIconsAction> {
-    return (dispatch: Redux.Dispatch<ReceiveIconsAction>) => {
-        return fetchIconsDispatchDebounced(dispatch, iconStyle, fetchFrom, fetchTo, searchText);
-    };
 }
 
 function fetchIcon(id: string, iconStyle: IconStyle): (dispatch: Redux.Dispatch<SelectedIconAction>) => Promise<SelectedIconAction> {
