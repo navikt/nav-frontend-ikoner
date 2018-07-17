c964aefe3bb96469a98f56a2faf8f913d82f7df6
Gunvor Lemvik <gunvor.lemvik@bekk.no>
2018-07-17 15:45:48 +0200

    FO-1333 Create buttons for existing extensions only

--- src/components/misc/download-types.tsx
+++ src/components/misc/download-types.tsx
@@ -12,50 +12,20 @@ interface PropTypes {
 class DownloadTypes extends React.Component<PropTypes> {
 
     public render() {
+        const style = this.props.iconStyle === IconStyle.FILLED ? Language.FILLED_ICON : Language.LINE_ICON;
 
-        const {icon, iconStyle} = this.props;
-        const style = iconStyle === IconStyle.FILLED ? Language.FILLED_ICON : Language.LINE_ICON;
-
-        const svg = "svg";
-        const svgExist = icon.locations
-                .filter(location => location.path.includes(style))
-                .filter(location => location.extension === svg).length > 0;
-
-        const png = "png";
-        const pngExist = this.props.icon.locations
-                .filter(location => location.path.includes(style))
-                .filter(location => location.extension === png).length > 0;
-
-        const pdf = "pdf";
-        const pdfExist = this.props.icon.locations
-                .filter(location => location.path.includes(style))
-                .filter(location => location.extension === pdf).length > 0;
-
-        const ai = "ai";
-        const aiExist = this.props.icon.locations
-                .filter(location => location.path.includes(style))
-                .filter(location => location.extension === ai).length > 0;
-
-        const sketch = "sketch";
-        const sketchExist = this.props.icon.locations
-                .filter(location => location.path.includes(style))
-                .filter(location => location.extension === sketch).length > 0;
-
-        const eps = "eps";
-        const epsExist = this.props.icon.locations
-                .filter(location => location.path.includes(style))
-                .filter(location => location.extension === eps).length > 0;
+        // Find all unique extensions of current icon
+        const duplicateExtensions: string[] = [];
+        this.props.icon.locations
+            .filter(location => location.path.toLowerCase().includes(style.toLowerCase()))
+            .map(location => duplicateExtensions.push(location.extension));
+        const uniqueExtensions = Array.from(new Set(duplicateExtensions));
 
         return (
-            <div>
-                <div className="download-types-row">
-                    {svgExist && <ExtensionButton extension={svg} icon={icon} style={style}/>}
-                    {pngExist && <ExtensionButton extension={png} icon={icon} style={style}/>}
-                    {pdfExist && <ExtensionButton extension={pdf} icon={icon} style={style}/>}
-                    {aiExist && <ExtensionButton extension={ai} icon={icon} style={style}/>}
-                    {sketchExist && <ExtensionButton extension={sketch} icon={icon} style={style}/>}
-                    {epsExist && <ExtensionButton extension={eps} icon={icon} style={style}/>}
-                </div>
+            <div className="download-types-row">
+                {uniqueExtensions.map((extension, index) =>
+                    <ExtensionButton key={index} extension={extension} icon={this.props.icon} style={style}/>
+                )}
             </div>
         );
     }
