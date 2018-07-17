93da8361f74c2617131bd08b851645bf6a0fa8e3
Gunvor Lemvik <gunvor.lemvik@bekk.no>
2018-07-17 15:51:26 +0200

    FO-1333 Clean up of code

--- src/components/misc/download-types.tsx
+++ src/components/misc/download-types.tsx
@@ -13,13 +13,9 @@ class DownloadTypes extends React.Component<PropTypes> {
 
     public render() {
         const style = this.props.iconStyle === IconStyle.FILLED ? Language.FILLED_ICON : Language.LINE_ICON;
-
-        // Find all unique extensions of current icon
-        const duplicateExtensions: string[] = [];
-        this.props.icon.locations
+        const uniqueExtensions = Array.from(new Set(this.props.icon.locations
             .filter(location => location.path.toLowerCase().includes(style.toLowerCase()))
-            .map(location => duplicateExtensions.push(location.extension));
-        const uniqueExtensions = Array.from(new Set(duplicateExtensions));
+            .map(location => location.extension)));
 
         return (
             <div className="download-types-row">
