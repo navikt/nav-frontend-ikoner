0345ff75890589ab41cdd43c5b173023e30d7b1d
Gunvor Lemvik <gunvor.lemvik@bekk.no>
2018-07-17 11:12:34 +0200

    FO-1333 Styled the buttons as nav --flat --mini

--- src/components/misc/download-types.tsx
+++ src/components/misc/download-types.tsx
@@ -13,37 +13,36 @@ class DownloadTypes extends React.Component<PropTypes> {
 
     public render() {
         const style = this.props.iconStyle === IconStyle.FILLED ? Language.FILLED_ICON : Language.LINE_ICON;
-        const svg = "svg"
-        const png = "png"
-        const pdf = "pdf"
-        const ai = "ai"
-        const sketch = "sketch"
-        const eps = "eps"
+        const svg = "svg";
+        const png = "png";
+        const pdf = "pdf";
+        const ai = "ai";
+        const sketch = "sketch";
+        const eps = "eps";
         return (
-            <div className="download-types-row">
-                <a className="button"
-                   href={`${Config.NAV_ICONS_API_LINK}/icon/download/extension?style=${style}&title=${this.props.icon.title}&extension=${svg}`}
-                   download={true}>{svg}</a>
-
-                <a className="button"
-                   href={`${Config.NAV_ICONS_API_LINK}/icon/download/extension?style=${style}&title=${this.props.icon.title}&extension=${png}`}
-                   download={true}>{png}</a>
-
-                <a className="button"
-                   href={`${Config.NAV_ICONS_API_LINK}/icon/download/extension?style=${style}&title=${this.props.icon.title}&extension=${pdf}`}
-                   download={true}>{pdf}</a>
-
-                <a className="button"
-                   href={`${Config.NAV_ICONS_API_LINK}/icon/download/extension?style=${style}&title=${this.props.icon.title}&extension=${ai}`}
-                   download={true}>{ai}</a>
-
-                <a className="button"
-                   href={`${Config.NAV_ICONS_API_LINK}/icon/download/extension?style=${style}&title=${this.props.icon.title}&extension=${sketch}`}
-                   download={true}>{sketch}</a>
-
-                <a className="button"
-                   href={`${Config.NAV_ICONS_API_LINK}/icon/download/extension?style=${style}&title=${this.props.icon.title}&extension=${eps}`}
-                   download={true}>{eps}</a>
+            <div>
+                <div className="download-types-row">
+                    <a className="knapp knapp--flat knapp--mini"
+                       href={`${Config.NAV_ICONS_API_LINK}/icon/download/extension?style=${style}&title=${this.props.icon.title}&extension=${svg}`}
+                       download={true}>{svg}</a>
+                    <a className="knapp knapp--flat knapp--mini"
+                       href={`${Config.NAV_ICONS_API_LINK}/icon/download/extension?style=${style}&title=${this.props.icon.title}&extension=${png}`}
+                       download={true}>{png}</a>
+                    <a className="knapp knapp--flat knapp--mini"
+                       href={`${Config.NAV_ICONS_API_LINK}/icon/download/extension?style=${style}&title=${this.props.icon.title}&extension=${pdf}`}
+                       download={true}>{pdf}</a>
+                </div>
+                <div className="download-types-row">
+                    <a className="knapp knapp--flat knapp--mini"
+                       href={`${Config.NAV_ICONS_API_LINK}/icon/download/extension?style=${style}&title=${this.props.icon.title}&extension=${ai}`}
+                       download={true}>{ai}</a>
+                    <a className="knapp knapp--flat knapp--mini"
+                       href={`${Config.NAV_ICONS_API_LINK}/icon/download/extension?style=${style}&title=${this.props.icon.title}&extension=${sketch}`}
+                       download={true}>{sketch}</a>
+                    <a className="knapp knapp--flat knapp--mini"
+                       href={`${Config.NAV_ICONS_API_LINK}/icon/download/extension?style=${style}&title=${this.props.icon.title}&extension=${eps}`}
+                       download={true}>{eps}</a>
+                </div>
             </div>
         );
     }
