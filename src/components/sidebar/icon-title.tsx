6238a058c42aa46220c76ff5cc5b9cbb60a89e04
Morten Jansrud <morten.jansrud@bekk.no>
2018-07-19 09:15:06 +0200

    FO-1273 Titlefix

--- src/components/sidebar/icon-title.tsx
+++ src/components/sidebar/icon-title.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
+import TextareaAutosize from 'react-autosize-textarea';
 import * as Redux from "react-redux";
-import {Input} from "../../../node_modules/nav-frontend-skjema";
 import {IconExpanded, IconStyle, Store, Tags} from "../../redux/store-interfaces";
 import api from "../../utils/api";
 import '../misc/misc.less';
+import './icon-title.less';
 import './tags.less';
 
 interface PropTypes {
@@ -31,13 +32,16 @@ class IconTitle extends React.Component<PropTypes, StateTypes>{
 
         return (
             <div className="icon-title-container">
-                <Input inputClassName="icon-title" label="" value={selectedIcon.title}
-                       onChange={this.handleTitleChange}/>
+                <TextareaAutosize
+                    className="icon-title"
+                    value={selectedIcon.title}
+                    onChange={this.handleTitleChange}
+                />
             </div>
         );
     }
 
-    private handleTitleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    private handleTitleChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
         this.props.editIcon(this.props.selectedIcon.id, event.target.value, this.props.selectedIcon.description, this.props.iconStyle);
     }
 }
