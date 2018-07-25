7e6962d5d77c59d7c6f43120bddc8845cd9d4200
Gunvor Lemvik <gunvor.lemvik@bekk.no>
2018-07-25 09:54:35 +0200

    FO-1237 Disabled extension-checkboxes for non-svg extensions when other color than default color has been specified

--- src/components/sidebar/icon-download-types.tsx
+++ src/components/sidebar/icon-download-types.tsx
@@ -1,6 +1,7 @@
 import Checkbox from "nav-frontend-skjema/lib/checkbox";
 import * as React from "react";
 import * as Redux from "react-redux";
+import * as ReactTooltip from 'react-tooltip';
 import Language from "../../language/norwegian";
 import {toggleChosenExtension} from "../../redux/actions";
 import {getChosenExtensions} from "../../redux/selectors";
@@ -10,11 +11,12 @@ import './icon-download-types.less';
 interface PropTypes {
     chosenExtensions: string[];
     icon: IconExpanded;
+    iconColor: string;
     iconStyle: IconStyle;
     toggleChosenExtension: typeof toggleChosenExtension;
 }
 
-function findUniqueLocations(icon: IconExpanded, style: IconStyle) {
+function findUniqueExtensions(icon: IconExpanded, style: IconStyle) {
     const stylePath = (style === IconStyle.FILLED ? Language.FILLED_ICON : Language.LINE_ICON).toLowerCase();
     const locations = icon.locations
         .filter((location) => location.path.toLowerCase().includes(stylePath))
@@ -24,16 +26,29 @@ function findUniqueLocations(icon: IconExpanded, style: IconStyle) {
 }
 
 function IconDownloadTypes(props: PropTypes) {
-    const uniqueLocations = findUniqueLocations(props.icon, props.iconStyle);
-    const checkboxes = uniqueLocations.map((extension, index) => (
-        <Checkbox
-            key={index}
-            label={extension}
-            id={extension}
-            checked={props.chosenExtensions.indexOf(extension) >= 0}
-            onChange={props.toggleChosenExtension}
-        />
+    const uniqueExtensions = findUniqueExtensions(props.icon, props.iconStyle);
+    const defaultColor = "black";
+    const checkboxes = uniqueExtensions.map((extension, index) => (
+        <div key={index}>
+            <a data-tip="" data-for={extension}>
+                <Checkbox
+                    label={extension}
+                    id={extension}
+                    disabled={extension !== "svg" && props.iconColor !== defaultColor}
+                    checked={props.chosenExtensions.indexOf(extension) >= 0}
+                    onChange={props.toggleChosenExtension}
+                />
+            </a>
+            {extension !== "svg" && props.iconColor !== defaultColor &&
+                <ReactTooltip id={extension} place="top" type="dark" effect="solid">
+                    <span>Finnes bare i svart</span>
+                </ReactTooltip>
+            }
+        </div>
     ));
+
+    console.log(uniqueExtensions)
+    console.log(props.chosenExtensions)
 
     return (
         <div className="download-types-row">
@@ -46,7 +61,8 @@ const mapStateToProps = (state: Store) => {
     return {
         chosenExtensions: getChosenExtensions(state),
         icon: state.iconsStore.selectedIcon,
-        iconStyle: state.iconsStore.iconStyle,
+        iconColor: state.iconsStore.iconColor,
+        iconStyle: state.iconsStore.iconStyle
     };
 };
 
