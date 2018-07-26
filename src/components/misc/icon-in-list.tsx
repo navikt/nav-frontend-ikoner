7ec0682e6f5cc79e7b7fc2cd353969b8cade4e53
Gunvor Lemvik <gunvor.lemvik@bekk.no>
2018-07-26 13:17:19 +0200

    KAIZEN-0 Cleaned up code

--- src/components/misc/icon-in-list.tsx
+++ src/components/misc/icon-in-list.tsx
@@ -8,20 +8,20 @@ interface PropTypes {
     selected: boolean;
 }
 
-class IconInList extends React.PureComponent<PropTypes> {
-    public render() {
-        const {icon, selected} = this.props;
-        const className = `icon-in-list ${selected ? 'icon-in-list-selected' : ''}`;
-        const iconLink =  `url(${icon.link})`;
-        const style = {backgroundImage: icon.extension === "png" ||  icon.extension === "svg"
-                ? iconLink
-                : undefined };
-        return (
-            <div className={className} style={style}>
-                <IconUnknownExtension extension={icon.extension}/>
-            </div>
-        );
-    }
+function IconInList(props: PropTypes) {
+    const {icon, selected} = props;
+    const className = `icon-in-list ${selected ? 'icon-in-list-selected' : ''}`;
+    const iconLink = `url(${icon.link})`;
+    const style = {
+        backgroundImage: icon.extension === "png" || icon.extension === "svg"
+            ? iconLink
+            : undefined
+    };
+    return (
+        <div className={className} style={style}>
+            <IconUnknownExtension extension={icon.extension}/>
+        </div>
+    );
 }
 
 export default IconInList;
